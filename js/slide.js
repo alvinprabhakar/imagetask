e78c6e8b7a9ca27a2197212822f3f3c85199d7b7
Alvin Prabha <alvinprabhakar@gmail.com>
2021-08-12 17:14:38 +0530

    Initial Commit

--- js/slide.js
+++ js/slide.js
@@ -1,40 +1,60 @@
-let i = 0; let changeImage=0;
+let i = 0; let changeImage=0; let images = ["NS1.jpg", "NS2.jpg","NS3.jpg","NS4.jpg","NS5.jpg"];
 
 function change() {
-  let color = ["NS1.jpg", "NS2.jpg","NS3.jpg","NS4.jpg","NS5.jpg"];
   
-  console.log(color[i]);
-  if(i===color.length){
+  
+  console.log(images[i]);
+  if(i===images.length){
     i = 0;
     }
-    document.body.style.backgroundImage = "url('images/"+color[i]+"')";
+    document.getElementById('image').src = "images/"+images[i]+"";
     console.log(i);
    i++;
  }
 
- startSlide();
+ changeImage = setInterval(() => change(), 2000);
+ 
  function startSlide(){
 
-   if(changeImage === 0){
+   //if(changeImage === 0){
      console.log("Invoking");
     changeImage = setInterval(() => change(), 2000);
-   }
+   //}
   }
 
 function stopSlide(){
     clearInterval(changeImage);
-    changeImage = 0;
+    
 }
 
 function prevImage(){
-    console.log("prev button");
-    i = i - 2;
-      change();
+    console.log("prev button",i);
+    clearInterval(changeImage);
+  if(i !== 0){
+    i = i - 1;
+    document.getElementById('image').src = "images/"+images[i]+"";
+  }
+  else{
+    i = images.length;
+    document.getElementById('image').src = "images/"+images[i]+"";
+  }
+  changeImage = setInterval(() => change(), 2000);
   }
 
 function nextImage(){
-  console.log("next button");
-    change();
-}
+  console.log("next button",i);
+  clearInterval(changeImage);
+  if(i>= images.length){
+    i = 0;
+    document.getElementById('image').src = "images/"+images[i]+"";
+  }
+  else{
+    i = i+1;
+    document.getElementById('image').src = "images/"+images[i]+"";
+  }
+  changeImage = setInterval(() => change(), 2000);
+  }
+    
+
 
 
